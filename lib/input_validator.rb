162801a073646d136081baaacb1cfdee38f38df7
Shen Sat <shensat18@gmail.com>
2019-07-20 09:42:07 +0100

    Add check for page number format

--- lib/input_validator.rb
+++ lib/input_validator.rb
@@ -1,6 +1,6 @@
 class InputValidator
 	def valid_page?(page)
-		page.match(/^\/[a-z]+_?[a-z]+$/)
+		page.match(/^\/[a-z]+_?[a-z]+(\/[1-9]+)?$/)
 		
 	end
 end
--- spec/input_validator_spec.rb
+++ spec/input_validator_spec.rb
@@ -2,7 +2,7 @@ require_relative '../lib/input_validator'
 
 describe 'InputValidator' do
 
-	it 'should check if page is in correct format' do
+	it 'should check if page section is in correct format' do
 		input_validator = InputValidator.new
 		correct_page_one = '/contact'
 		correct_page_two = '/help_page'
@@ -13,5 +13,19 @@ describe 'InputValidator' do
 		expect(input_validator.valid_page?(incorrect_page_one)).to be_falsey
 		expect(input_validator.valid_page?(incorrect_page_two)).to be_falsey
 	end
+
+	it 'should check if page number is in correct format' do
+		input_validator = InputValidator.new
+		correct_page_one = '/about/1'
+		correct_page_two = '/help_page/22'
+		incorrect_page_one = '/about/'
+		incorrect_page_two = '/about/a'
+		expect(input_validator.valid_page?(correct_page_one)).to be_truthy
+		expect(input_validator.valid_page?(correct_page_two)).to be_truthy
+		expect(input_validator.valid_page?(incorrect_page_one)).to be_falsey
+		expect(input_validator.valid_page?(incorrect_page_two)).to be_falsey
+	end
+
+
 
 end
