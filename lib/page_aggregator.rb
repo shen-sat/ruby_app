ba1efbe3a0acda6a1d761b65224d469bd06901e3
Shen Sat <shensat18@gmail.com>
2019-07-21 15:51:47 +0100

    Add aggregate method, adds pages to total views

--- lib/page_aggregator.rb
+++ lib/page_aggregator.rb
@@ -2,7 +2,14 @@ class PageAggregator
 	attr_accessor :total_views, :unique_views
 
 	def initialize
-		@total_views = []
-		@unique_views = []
+		@total_views = {}
+		@unique_views = {}
+	end
+
+	def aggregate(logs)
+		logs.each do |log|
+			page = log.split(' ')[0]
+			@total_views[page].nil? ? @total_views[page] = 1 : @total_views[page] += 1 
+		end
 	end
 end
--- spec/page_aggregator_spec.rb
+++ spec/page_aggregator_spec.rb
@@ -4,7 +4,26 @@ describe 'PageAggregator' do
 
 	it 'initialize with empty lists for total views and unique views' do
 		page_aggregator = PageAggregator.new
-		expect(page_aggregator.total_views).to eq([])
-		expect(page_aggregator.unique_views).to eq([])
+		expect(page_aggregator.total_views).to eq({})
+		expect(page_aggregator.unique_views).to eq({})
 	end
+
+	it 'should record a log in total views' do
+		page_aggregator = PageAggregator.new
+		log = '/contact 184.123.665.067'
+		logs = [log]
+		page_aggregator.aggregate(logs)
+		expect(page_aggregator.total_views).to eq({ '/contact' => 1 }) 
+	end
+
+	it 'should record two logs with same page in total views' do
+		page_aggregator = PageAggregator.new
+		log_one = '/contact 184.123.665.067'
+		log_two = '/contact 184.123.665.067'
+		logs = [log_one, log_two]
+		page_aggregator.aggregate(logs)
+		expect(page_aggregator.total_views).to eq({ '/contact' => 2 }) 
+	end
+
+
 end
