85d4d5ecafd2cec4942c9684906c5ea84744ea9d
Shen Sat <shensat18@gmail.com>
2019-07-21 20:52:45 +0100

    Refactor app class to initialize with input validator and page aggregator

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,8 @@
 class App
 
+	def initialize(input_validator, page_aggregator)
+	end
+
 	def run(file)
 		puts "Please provide correct file" if file.nil? || !File.exists?(file)
 	end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -2,18 +2,22 @@ require_relative '../lib/app.rb'
 
 describe 'App' do
 
+	before(:each) do
+ 		input_validator = double('input_validator')   	
+ 		page_aggregator = double('page_aggregator')   	
+    	@app = App.new(input_validator, page_aggregator)
+  	end
+
 	it 'should puts an error message if file is not provided' do
-		app = App.new
 		file = double('file')
 		allow(file).to receive(:nil?).and_return(true)
-		expect { app.run file }.to output("Please provide correct file\n").to_stdout
+		expect { @app.run file }.to output("Please provide correct file\n").to_stdout
 	end
 
 	it 'should puts an error message if file does not exist' do
-		app = App.new
 		file = double('file')
 		allow(File).to receive(:exists?).and_return(false)
-		expect { app.run file }.to output("Please provide correct file\n").to_stdout
+		expect { @app.run file }.to output("Please provide correct file\n").to_stdout
 	end
 
 end
