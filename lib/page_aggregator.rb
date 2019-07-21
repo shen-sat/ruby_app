5b5f2b0ba9961ddf604884101e6913157d8d43c3
Shen Sat <shensat18@gmail.com>
2019-07-21 16:36:37 +0100

    Refactor page aggregator to have smaller methods

--- lib/page_aggregator.rb
+++ lib/page_aggregator.rb
@@ -11,11 +11,19 @@ class PageAggregator
 		logs.each do |log|
 			page = log.split(' ')[0]
 			address = log.split(' ')[1]
-			@total_views[page].nil? ? @total_views[page] = 1 : @total_views[page] += 1
-			if @history[page] != address
-				@history[page] = address 
-				@unique_views[page].nil? ? @unique_views[page] = 1 : @unique_views[page] += 1
-			end
+			add_to_total(page)
+			add_to_unique(page, address)
+		end
+	end
+
+	def add_to_total(page)
+		@total_views[page].nil? ? @total_views[page] = 1 : @total_views[page] += 1
+	end
+
+	def add_to_unique(page, address)
+		if @history[page] != address
+			@history[page] = address 
+			@unique_views[page].nil? ? @unique_views[page] = 1 : @unique_views[page] += 1
 		end
 	end
 end
