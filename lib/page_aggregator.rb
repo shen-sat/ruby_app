98b0096a855952efc7f225d9823ba997c6557571
Shen Sat <shensat18@gmail.com>
2019-07-21 16:26:43 +0100

    Add pages to unique views depending on address

--- lib/page_aggregator.rb
+++ lib/page_aggregator.rb
@@ -4,12 +4,18 @@ class PageAggregator
 	def initialize
 		@total_views = {}
 		@unique_views = {}
+		@history = {}
 	end
 
 	def aggregate(logs)
 		logs.each do |log|
 			page = log.split(' ')[0]
-			@total_views[page].nil? ? @total_views[page] = 1 : @total_views[page] += 1 
+			address = log.split(' ')[1]
+			@total_views[page].nil? ? @total_views[page] = 1 : @total_views[page] += 1
+			if @history[page] != address
+				@history[page] = address 
+				@unique_views[page].nil? ? @unique_views[page] = 1 : @unique_views[page] += 1
+			end
 		end
 	end
 end
