527bac4a5b54a48b3f315ae83ce9639672464fff
Shen Sat <shensat18@gmail.com>
2019-07-22 21:09:45 +0100

    Extract checking of file existence into its own method

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,11 @@
 class App
 
 	def initialize(input_validator, page_aggregator)
+		@input_validator = input_validator
+		@page_aggregator = page_aggregator
 	end
 
-	def run(file)
+	def check_file(file)
 		puts "Please provide correct file" if file.nil? || !File.exists?(file)
 	end
 
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,21 +3,21 @@ require_relative '../lib/app.rb'
 describe 'App' do
 
 	before(:each) do
- 		input_validator = double('input_validator')   	
- 		page_aggregator = double('page_aggregator')   	
-    	@app = App.new(input_validator, page_aggregator)
+ 		@input_validator = double('input_validator')   	
+ 		@page_aggregator = double('page_aggregator')   	
+    	@app = App.new(@input_validator, @page_aggregator)
   	end
 
 	it 'should puts an error message if file is not provided' do
 		file = double('file')
 		allow(file).to receive(:nil?).and_return(true)
-		expect { @app.run file }.to output("Please provide correct file\n").to_stdout
+		expect { @app.check_file file }.to output("Please provide correct file\n").to_stdout
 	end
 
 	it 'should puts an error message if file does not exist' do
 		file = double('file')
 		allow(File).to receive(:exists?).and_return(false)
-		expect { @app.run file }.to output("Please provide correct file\n").to_stdout
+		expect { @app.check_file file }.to output("Please provide correct file\n").to_stdout
 	end
-
+	
 end
