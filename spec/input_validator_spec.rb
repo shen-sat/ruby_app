3294f2e8bf269d85c31f5ce20cf624f4d4e0ea58
Shen Sat <shensat18@gmail.com>
2019-07-23 01:40:53 +0100

    Refactor input validator tests

--- spec/input_validator_spec.rb
+++ spec/input_validator_spec.rb
@@ -2,13 +2,15 @@ require_relative '../lib/input_validator'
 
 describe 'InputValidator' do
 
+	before(:each) do
+ 		@input_validator = InputValidator.new
+  	end
+
 	it 'should initialize with empty lists of correctly formatted and incorrect formatted logs' do
-		input_validator = InputValidator.new
-		expect(input_validator.correctly_formatted_logs).to eq([])
+		expect(@input_validator.correctly_formatted_logs).to eq([])
 	end
 
 	it 'should check log is in correct format' do
-		input_validator = InputValidator.new
 		valid_log_one = '/contact 184.123.665.067'
 		valid_log_two = '/help_page/1 126.318.035.038'
 		valid_log_three = '/help_page/22 126.318.035.038'
@@ -21,26 +23,25 @@ describe 'InputValidator' do
 		invalid_log_seven = '/help_page/1 126'
 		invalid_log_eight = '/help_page/1 126.318'
 		invalid_log_nine = '/help_page/1 126.318.035'
-		expect(input_validator.valid_log?(valid_log_one)).to be_truthy
-		expect(input_validator.valid_log?(valid_log_two)).to be_truthy
-		expect(input_validator.valid_log?(valid_log_three)).to be_truthy
-		expect(input_validator.valid_log?(invalid_log_one)).to be_falsey
-		expect(input_validator.valid_log?(invalid_log_two)).to be_falsey
-		expect(input_validator.valid_log?(invalid_log_three)).to be_falsey
-		expect(input_validator.valid_log?(invalid_log_four)).to be_falsey
-		expect(input_validator.valid_log?(invalid_log_five)).to be_falsey
-		expect(input_validator.valid_log?(invalid_log_six)).to be_falsey
-		expect(input_validator.valid_log?(invalid_log_seven)).to be_falsey
-		expect(input_validator.valid_log?(invalid_log_eight)).to be_falsey
-		expect(input_validator.valid_log?(invalid_log_nine)).to be_falsey
+		expect(@input_validator.valid_log?(valid_log_one)).to be_truthy
+		expect(@input_validator.valid_log?(valid_log_two)).to be_truthy
+		expect(@input_validator.valid_log?(valid_log_three)).to be_truthy
+		expect(@input_validator.valid_log?(invalid_log_one)).to be_falsey
+		expect(@input_validator.valid_log?(invalid_log_two)).to be_falsey
+		expect(@input_validator.valid_log?(invalid_log_three)).to be_falsey
+		expect(@input_validator.valid_log?(invalid_log_four)).to be_falsey
+		expect(@input_validator.valid_log?(invalid_log_five)).to be_falsey
+		expect(@input_validator.valid_log?(invalid_log_six)).to be_falsey
+		expect(@input_validator.valid_log?(invalid_log_seven)).to be_falsey
+		expect(@input_validator.valid_log?(invalid_log_eight)).to be_falsey
+		expect(@input_validator.valid_log?(invalid_log_nine)).to be_falsey
 	end
 
 	it 'should save a correct log to the list of correctly formatted logs' do
-		input_validator = InputValidator.new
 		log = double('log')
-		allow(input_validator).to receive(:valid_log?).and_return(true)
-		input_validator.save_log_to_list(log)
-		expect(input_validator.correctly_formatted_logs).to eq([log])
+		allow(@input_validator).to receive(:valid_log?).and_return(true)
+		@input_validator.save_log_to_list(log)
+		expect(@input_validator.correctly_formatted_logs).to eq([log])
 	end
 
 end
